0021e0878161ebf12cf9fd852481ad4196d7ae69
Ritesh Mishra <riteshmishra9565@gmail.com>
2025-09-27 20:01:26 +0530

    blogs schema done

--- models/Blogs.js
+++ models/Blogs.js
@@ -1,6 +1,81 @@
 const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({});
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 150,
+    },
+
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // blog kisne likha hai
+      required: true,
+    },
+
+    categories: {
+      type: [String],
+      default: [],
+    },
+
+    tags: {
+      type: [String],
+      default: [],
+    },
+
+    thumbnail: {
+      type: String,
+    },
+
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User", // jisne like kiya
+      },
+    ],
+
+    comments: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+        text: {
+          type: String,
+          required: true,
+          trim: true,
+          maxLength: 500,
+        },
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+
+    isPublished: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
 const Blog = mongoose.model("Blog", blogSchema);
 module.exports = Blog;
