40caed94ed1fc55ed53d800fb719332d30270149
Ritesh Mishra <riteshmishra9565@gmail.com>
2025-10-16 20:33:06 +0530

    debugging the main sending in deployment

--- utils/mailSender.js
+++ utils/mailSender.js
@@ -5,7 +5,7 @@ exports.mailSender = async (email, title, body) => {
     let transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
       port: 465,
-      secure: process.env.MAIL_SECURE === "true",
+      secure: false,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
